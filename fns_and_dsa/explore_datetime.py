92c08aaf18798dab265fb965961f27ae2cde4225
Damilare Ajisafe <dammyajisafe@gmail.com>
2025-07-26 10:53:15 +0100

    modify future date output format to use strftime in explore_datetime.py

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -9,7 +9,7 @@ def calculate_future_date():
     num_of_days = int(input("Enter the number of days to add to the current date: "))
     today = datetime.date.today()
     future_date = today + datetime.timedelta(days=num_of_days)
-    print(f"Future date: {future_date}")
+    print(f"Future date: {future_date.strftime("%Y-%m-%d %H:%M:%S")}")
 
 
 
