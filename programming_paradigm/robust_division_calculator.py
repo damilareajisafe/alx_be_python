c87efee7974bbbb61bdafefa3f722af8ea6348b3
damilareajisafe <dammyajisafe@gmail.com>
2025-10-08 17:24:06 +0100

    add simple_calculator.py and test_simple_calculator.py

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -2,7 +2,7 @@ def safe_divide(numerator, denominator):
     try:
         result = float(numerator) / float(denominator)
         return f"The result of the division is {result}"
-    except ValueError:
+    except ValueError as e:
         return "Error: Please enter numeric values only."
     except ZeroDivisionError:
         return "Error: Cannot divide by zero."
---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,32 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+
+    def setUp(self):
+        self.calc = SimpleCalculator()
+
+    def test_addition(self):
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(1, -1), 0)
+
+    def test_subtraction(self):
+        self.assertEqual(self.calc.subtract(2, 3), -1)
+        self.assertEqual(self.calc.subtract(1, -1), 2)
+
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(2, 3), 6)
+        self.assertEqual(self.calc.multiply(1, -1), -1)
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(10, 2), 5)
+        self.assertEqual(self.calc.divide(-9, 3), -3)
+        self.assertEqual(self.calc.divide(5, 2), 2.5)
+
+    def test_divideByZero(self):
+        self.assertIsNone(self.calc.divide(10, 0))
+        # with self.assertRaises(ZeroDivisionError):
+        #     self.calc.divide(10, 0)
+
+if __name__ == "__main__":
+    unittest.main()
